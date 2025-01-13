3e0021bfed8dc235e8c149c4a7aa32d9caf66de1
JaviArrebola <24dw.francisco.arrebola@arangoya.net>
2025-01-13 17:11:18 +0100

    Corrección Atributos clase Libro. Modificación Constructor. -Javi A.

--- src/biblioteca/Libro.java
+++ src/biblioteca/Libro.java
@@ -3,18 +3,56 @@ package biblioteca;
 public class Libro {
 	private String titulo;
 	private String autor;
-	private String IDLibro;
+	private int IDLibro;
 	private int anioPublicacion;
 	private int cantidadCopias;
 	private Reserva[] reservas;
+	private static int contador = 0;
 	
-	public Libro(String titulo, String autor, String IDLibro, int anioPublicacion ,int cantidadCopias)
+	public Libro(String titulo, String autor, int anioPublicacion ,int cantidadCopias)
 	{
 		this.titulo = titulo;
 		this.autor = autor;
-		this.IDLibro = IDLibro;
+		this.IDLibro = contador;
 		this.anioPublicacion = anioPublicacion;
 		this.cantidadCopias = cantidadCopias;
+		contador++;
+		
+		
 	}
+	
+	public Libro registrarLibro(String titulo, String autor, int anioPublicacion ,int cantidadCopias)
+	{
+		
+	}
+	
+	public void consultarLibro(String nombre)
+	{
+		System.out.println();
+	}
+	
+	public void consultarLibro(int id)
+	{
+		System.out.println();
+	}
+	
+	
+	public void consultarLibro(int IDLibro )
+	{
+		
+	}
+	
+	public aniadirReserva() 
+	{
+		
+	}
+	
+	public eliminarReserva()
+	{
+		
+	}
+	
+	
+	
 
 }

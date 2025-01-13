14f73ef81f2b25bb615e289231aefe3b77de855c
B1LBQ <116708033+B1LBQ@users.noreply.github.com>
2025-01-13 09:29:21 +0100

    Update Usuario.java

--- Biblioteca/src/biblioteca/Usuario.java
+++ Biblioteca/src/biblioteca/Usuario.java
@@ -2,4 +2,37 @@ package biblioteca;
 
 public class Usuario {
 
+  package biblioteca;
+
+public class Usuarios {
+	
+	    private String nombre;
+	    private String id;
+	    private String rol; // Puede ser "admin", "usuario", etc.
+	    private String librosPrestados;
+
+	    // Constructor
+	    public void Usuario(String nombre, String id_unico, String rol) {
+	        this.nombre = nombre;
+	        this.id = id_unico;
+	        this.rol = rol;
+	        this.librosPrestados = librosPrestados;
+	    }
+
+	    // Métodos getters
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public String getId() {
+	        return id;
+	    }
+
+	    public String getRol() {
+	        return rol;
+	    }
+
+	    public String getLibrosPrestados() {
+	        return librosPrestados;
+      }
 }

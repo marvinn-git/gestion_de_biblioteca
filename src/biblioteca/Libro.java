d69c476d6cffb44f09b8429d32139781ca384227
ikasle <ikasle@DAW1-02.arangoya.local>
2025-01-13 09:41:49 +0100

    Inserción atributos Javi A.

--- src/biblioteca/Libro.java
+++ src/biblioteca/Libro.java
@@ -1,5 +1,11 @@
 package biblioteca;
 
 public class Libro {
+	private String titulo;
+	private String autor;
+	private String IDLibro;
+	private int anioPublicacion;
+	private int cantidadCopias;
+	private Reserva[] reservas;
 
 }

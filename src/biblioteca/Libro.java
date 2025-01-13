64af9238d73fdefdf0d3cfd299d15f773f21ae4f
ikasle <ikasle@DAW1-02.arangoya.local>
2025-01-13 09:49:51 +0100

    Creacción Constructor clase Libro -Javi A.

--- src/biblioteca/Libro.java
+++ src/biblioteca/Libro.java
@@ -7,5 +7,14 @@ public class Libro {
 	private int anioPublicacion;
 	private int cantidadCopias;
 	private Reserva[] reservas;
+	
+	public Libro(String titulo, String autor, String IDLibro, int anioPublicacion ,int cantidadCopias)
+	{
+		this.titulo = titulo;
+		this.autor = autor;
+		this.IDLibro = IDLibro;
+		this.anioPublicacion = anioPublicacion;
+		this.cantidadCopias = cantidadCopias;
+	}
 
 }

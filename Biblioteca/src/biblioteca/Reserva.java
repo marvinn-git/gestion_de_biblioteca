7a2436c04e994da33c71a8a1d694ed1f4cd81794
marvinn-git <24dw.marvin.pena@arangoya.net>
2025-01-15 13:20:49 +0100

    Marvin.java
    Constructor, getter y setter

--- Biblioteca/src/biblioteca/Reserva.java
+++ Biblioteca/src/biblioteca/Reserva.java
@@ -6,4 +6,45 @@ public class Reserva {
 	private libro libro;
 	private int fecha_reserva;
 
+	
+    // Constructor
+    public Reserva(int idReserva, String usuario, String libro, String fechaReserva) {
+        this.idReserva = idReserva;
+        this.usuario = usuario;
+        this.libro = libro;
+        this.fechaReserva = fechaReserva;
+    }
+
+    // Getter y Setter 
+    public int getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(int idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getLibro() {
+        return libro;
+    }
+
+    public void setLibro(String libro) {
+        this.libro = libro;
+    }
+
+    public String getFechaReserva() {
+        return fechaReserva;
+    }
+
+    public void setFechaReserva(String fechaReserva) {
+        this.fechaReserva = fechaReserva;
+    }
 }

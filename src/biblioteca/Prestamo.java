43408bee46f259051321d969a6cad492d3236dae
diegoolaba <24dw.diego.olabarrieta@arangoya.net>
2025-01-16 12:15:40 +0100

    Update Prestamo.java

--- src/biblioteca/Prestamo.java
+++ src/biblioteca/Prestamo.java
@@ -1,5 +1,11 @@
 package biblioteca;
 
 public class Prestamo {
-
+public class Prestamo {
+	    // Atributos
+	    private String idPrestamo;
+	    private String libro;
+	    private String usuario;
+	    private int fechaPrestamo;
+	    private int fechaDevolucion;
 }

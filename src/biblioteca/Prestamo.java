4872433d128887eb6e48bf455232a09cac691ab0
diegoolaba <24dw.diego.olabarrieta@arangoya.net>
2025-01-16 12:16:24 +0100

    Update Prestamo.java

--- src/biblioteca/Prestamo.java
+++ src/biblioteca/Prestamo.java
@@ -8,4 +8,10 @@ public class Prestamo {
 	    private String usuario;
 	    private int fechaPrestamo;
 	    private int fechaDevolucion;
+public Prestamo(int ID_prestamo, String libro, String usuario, int fecha_prestamo, int fecha_devolucion) {
+	this.ID_prestamo = ID_prestamo;
+	this.libro = libro;		this.usuario = usuario;
+	this.fecha_prestamo = fecha_prestamo;
+	this.fecha_devolucion = fecha_devolucion;	
+	}
 }

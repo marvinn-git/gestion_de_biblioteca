429ee58c0bcb050c6e6efa25f460af90563bf05e
marvinn-git <24dw.marvin.pena@arangoya.net>
2025-01-16 12:42:40 +0100

    Update Usuario.java
    Commit de eenko para realizar la clase usuario

--- Biblioteca/src/biblioteca/Usuario.java
+++ Biblioteca/src/biblioteca/Usuario.java
@@ -1,7 +1,8 @@
 package biblioteca;
 
 public class Usuarios {
-	
+
+  //Atributos
   private String nombre;
   private String id_unico;
   private String rol;
@@ -33,5 +34,23 @@ public class Usuarios {
 }
   public String getlibrosPrestados() {
 	return librosPrestados;
+  }
+}
+
+  //Metodos
+  public static Usuarios registrarUsuarios (String nombre, String id_unico, String rol, String librosPrestados) {
+	return new Usuarios ();
+	    }
+  public static void listarUsuarios(Usuarios Usuarios) {
+	for (Usuario usuario : usuarios) {
+        System.out.println("Nombre: " + usuario.nombre);
+        System.out.println("ID Único: " + usuario.idUnico);
+        System.out.println("Rol: " + usuario.rol);
+        System.out.println("Libros Prestados: " + usuario.librosPrestados);
+        System.out.println("---------------------------------");
+  }
+}
+  public boolean puedeRealizarPrestamo() {
+	return this.librosPrestados.size() < 5;
   }
 }

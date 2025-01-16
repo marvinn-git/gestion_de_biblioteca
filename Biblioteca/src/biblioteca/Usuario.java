702bbb986ef2eabebbf6ee55f8e27fc23163de37
B1LBQ <116708033+B1LBQ@users.noreply.github.com>
2025-01-16 11:59:16 +0100

    Update Usuario.java

--- Biblioteca/src/biblioteca/Usuario.java
+++ Biblioteca/src/biblioteca/Usuario.java
@@ -2,31 +2,36 @@ package biblioteca;
 
 public class Usuarios {
 	
-	    private String nombre;
-	    private String id_unico;
-	    private String rol;
-	    private String librosPrestados;
+  private String nombre;
+  private String id_unico;
+  private String rol;
+  private String librosPrestados;
 
-	    public void Usuario(String nombre, String id_unico, String rol, String librosPrestados) {
-	        this.nombre = nombre;
-	        this.id_unico = id_unico;
-	        this.rol = rol;
-	        this.librosPrestados = librosPrestados;
-	    }
+  public void Usuario(String nombre, String id_unico, String rol, String librosPrestados) {
+	this.nombre = nombre;
+	this.id_unico = id_unico;
+        this.rol = rol;
+        this.librosPrestados = librosPrestados;
+	    
+  }
 
-	    public String getnombre() {
-	        return nombre;
-	    }
+  public String getnombre() {
+        return nombre;
+  }
 
-	    public String getid_unico() {
-	        return id_unico;
-	    }
+  public String getid_unico() {
+        return id_unico;
+  }
 
-	    public String getrol() {
-	        return rol;
-	    }
+  public String getrol() {
+        return rol;
+  }
 
-	    public String getlibrosPrestados() {
-	        return librosPrestados;
-	    }
-	}
+  public String getlibrosPrestados() {
+        return librosPrestados;
+     }
+  }
+  public String getlibrosPrestados() {
+	return librosPrestados;
+     }
+  }
